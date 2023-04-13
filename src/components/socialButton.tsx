2c8dbdd215865b84427aea796b8005b19d4aa7bf
Santiago Hernández <68081080+santiagohernandezs@users.noreply.github.com>
2023-04-12 20:11:08 -0400

    🐛 fix hover animation in social buttons div

--- src/components/socialButton.tsx
+++ src/components/socialButton.tsx
@@ -15,7 +15,7 @@ export default function SocialButton({
       role='link'
       accessKey={alt.at(0)}
       aria-label={`link to my ${alt}`}
-      className='border-4 border-black/primary rounded-full w-20 h-16 bigPhone:w-24 bigPhone:h-14 flex justify-center items-center hover:w-28 transition-all ease-in'>
+      className='border-4 border-black/primary rounded-full w-20 h-16 bigPhone:w-24 bigPhone:h-14 flex justify-center items-center'>
       <img src={image} alt={alt} width={25} height={25} />
     </a>
   )
