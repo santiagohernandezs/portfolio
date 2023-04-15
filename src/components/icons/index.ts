ec0c29b4bfad1e80e9a462f618d96e37252dd230
Santiago Hernández <68081080+santiagohernandezs@users.noreply.github.com>
2023-04-15 13:07:47 -0400

    🍱 add ticket icon as a component

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,5 +1,6 @@
 export { default as Chevron } from './Chevron';
 export { default as Github } from './Github';
 export { default as Mail } from './Mail';
+export { default as Ticket } from './Ticket';
 export { default as Twitter } from './Twitter';
 
