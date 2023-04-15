ec5f5e2523998300cb9e9eb6018317044053a054
Santiago Hernández <68081080+santiagohernandezs@users.noreply.github.com>
2023-04-14 23:20:23 -0400

    🎨 add index file

---
+++ src/components/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as Chevron } from './Chevron';
+export { default as Github } from './Github';
+export { default as Mail } from './Mail';
+export { default as Twitter } from './Twitter';
+
